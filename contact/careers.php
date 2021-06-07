059a944e850846683d15aa42da8549a9168e6ff8
boxete-dev <76531734+boxete-dev@users.noreply.github.com>
2021-06-07 13:41:51 -0400

    Add files via upload

---
+++ contact/careers.php
@@ -0,0 +1,38 @@
+<?php
+// database connection code
+if(isset($_POST['txtFirstName']))
+{
+
+$con = mysqli_connect('localhost', 'jcl3217', 'Dl433123!','db_main');
+// get the post records
+
+$txtFirstName = $_POST['txtFirstName'];
+$txtLastName = $_POST['txtLastName'];
+$txtAddress = $_POST['txtAddress'];
+$txtCity = $_POST['txtCity'];
+$txtState = $_POST['txtState'];
+$txtZipcode = $_POST['txtZipcode'];
+$txtEmail = $_POST['txtEmail'];
+$txtPhone = $_POST['txtPhone'];
+$txtPhonetype = $_POST['txtPhonetype'];
+
+
+// database insert SQL code
+$sql = "INSERT INTO `tbl_career` (`id`, `fldFirstname`, `fldLastname`, `fldAddress`, `fldCity`, `fldState`, `fldZip`, `fldEmail`, `fldPhone`, `fldPhonetype`) VALUES ('0', '$txtFirstName','$txtLastName','$txtAddress', '$txtCity', '$txtState', '$txtZipcode', '$txtEmail', '$txtPhone', '$txtPhonetype')";
+
+// insert in database 
+$rs = mysqli_query($con, $sql);
+if($rs)
+{
+	
+
+	header("Location:http://boxete.com/careers.html"); 
+		echo '<script>alert("Thank you for part of Boxete!!")</script>';
+}
+}
+else
+{
+	echo "Are you a genuine visitor?";
+	
+}
+?>
---
+++ contact/driver.php
@@ -0,0 +1,39 @@
+<?php
+// database connection code
+if(isset($_POST['txtFirstName']))
+{
+
+$con = mysqli_connect('localhost', 'jcl3217', 'Dl433123!','db_main');
+// get the post records
+
+$txtFirstName = $_POST['txtFirstName'];
+$txtLastName = $_POST['txtLastName'];
+$txtAddress = $_POST['txtAddress'];
+$txtCity = $_POST['txtCity'];
+$txtState = $_POST['txtState'];
+$txtZipcode = $_POST['txtZipcode'];
+$txtEmail = $_POST['txtEmail'];
+$txtPhone = $_POST['txtPhone'];
+$txtPhonetype = $_POST['txtPhonetype'];
+$txtOtherCompany = $_POST['txtOtherCompany'];	
+
+
+// database insert SQL code
+$sql = "INSERT INTO `tbl_driver` (`id`, `fldFirstname`, `fldLastname`, `fldAddress`, `fldCity`, `fldState`, `fldZip`, `fldEmail`, `fldPhone`, `fldPhonetype`, `fldOthercompany`) VALUES ('0', '$txtFirstName','$txtLastName','$txtAddress', '$txtCity', '$txtState', '$txtZipcode', '$txtEmail', '$txtPhone', '$txtPhonetype', '$txtOtherCompany')";
+
+// insert in database 
+$rs = mysqli_query($con, $sql);
+if($rs)
+{
+	
+
+	header("Location:http://boxete.com/driver.html"); 
+		echo '<script>alert("Thank you for part of Boxete!!")</script>';
+}
+}
+else
+{
+	echo "Are you a genuine visitor?";
+	
+}
+?>
