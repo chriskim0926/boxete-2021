1036fdc1ffc6f924b9c1f18162be5f860d5ee63c
Jaechang Lee <jae@newcricketwireless.com>
2021-06-17 17:03:00 -0400

    changes

--- contact/careers.php
+++ contact/careers.php
@@ -1,8 +1,27 @@
 <?php
-include "../db/dbcareers.php";
 // database connection code
 if(isset($_POST['submit']))
 {
+	$con = mysqli_connect('localhost', 'jcl3217', 'Dl433123!','db_main');
+	// get the post records
+
+	$txtFirstName = $_POST['txtFirstName'];
+	$txtLastName = $_POST['txtLastName'];
+	$txtAddress = $_POST['txtAddress'];
+	$txtCity = $_POST['txtCity'];
+	$txtState = $_POST['txtState'];
+	$txtZipcode = $_POST['txtZipcode'];
+	$txtEmail = $_POST['txtEmail'];
+	$txtPhone = $_POST['txtPhone'];
+	$txtPhonetype = $_POST['txtPhonetype'];
+
+	// database insert SQL code
+	$sql = "INSERT INTO `tbl_career` (`id`, `fldFirstname`, `fldLastname`, `fldAddress`, `fldCity`, `fldState`, `fldZip`, `fldEmail`, `fldPhone`, `fldPhonetype`) VALUES ('0', '$txtFirstName','$txtLastName','$txtAddress', '$txtCity', '$txtState', '$txtZipcode', '$txtEmail', '$txtPhone', '$txtPhonetype')";
+
+	// insert in database 
+	$rs = mysqli_query($con, $sql);
+
+	
 	$to = "jae@newcricketwireless.com"; // this is your Email address
     $from = $_POST['txtEmail']; // this is the sender's Email address
     $$txtFirstName = $_POST['txtFirstName'];
@@ -12,9 +31,7 @@ if(isset($_POST['submit']))
 
     $headers = "From:" . $from;
     mail($to,$subject,$message,$headers);
-		$message = "";
-		 $message = "Success! You entered: ";
-   
+		
 }	
 
 
