dcef3479cd05be5266ff41e9fe93e413ae22af01
Jaechang Lee <jae@newcricketwireless.com>
2021-06-17 15:26:18 -0400

    changes

--- contact/careers.php
+++ contact/careers.php
@@ -27,11 +27,10 @@ $rs = mysqli_query($con, $sql);
     $$txtFirstName = $_POST['txtFirstName'];
     $txtLastName = $_POST['txtLastName'];
     $subject = "BOXETE - Career form submission";
-    $message = $txtFirstName . " " . $txtLastName . " We got an career submission from " . "\n\n Please check the submission details" . $_POST['message'];
+    $message =  "\nFull Name: " . $txtFirstName . " " . $txtLastName . "\nEmail: " . $txtEmail . "\nAddress:" . $txtAddress . "\n " . $txtCity . " " . $txtState . " " . $txtZipcode . "\n\nWe got an career submission from www.boxetepartner.com" . "\n\n Please check the submission details" . $_POST['message'];
 
     $headers = "From:" . $from;
     mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
     echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     }	
